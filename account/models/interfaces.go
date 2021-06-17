2074276b069dc0e11c5ace8185573204edc98d63
Reece Donovan <reecedonovan99@gmail.com>
2021-06-17 15:56:52 +0100

    Added contect to User interface methods

--- account/models/interfaces.go
+++ account/models/interfaces.go
@@ -1,13 +1,17 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
 
 // UserService defines methods the handler layer expects the service it interacts with to implement
 type UserService interface {
-	Get(uid uuid.UUID) (*User, error)
+	Get(ctx context.Context, uid uuid.UUID) (*User, error)
 }
 
 // UserRepository defines methods the service layer expects the repository it interacts with to implement
 type UserRepository interface {
-	FindByID(uid uuid.UUID) (*User, error)
+	FindByID(ctx context.Context, uid uuid.UUID) (*User, error)
 }
