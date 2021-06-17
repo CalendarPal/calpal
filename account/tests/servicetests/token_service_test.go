bf7e206faa53a4bce87e3f39777c3cbd41487199
Reece Donovan <reecedonovan99@gmail.com>
2021-06-17 22:15:47 +0100

    Testing for the token service

---
+++ account/tests/servicetests/token_service_test.go
@@ -0,0 +1,92 @@
+package servicetests
+
+import (
+	"context"
+	"io/ioutil"
+	"testing"
+	"time"
+
+	"github.com/CalendarPal/calpal-api/account/auth"
+	"github.com/CalendarPal/calpal-api/account/models"
+	"github.com/CalendarPal/calpal-api/account/services"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewPairFromUser(t *testing.T) {
+	priv, _ := ioutil.ReadFile("../../rsa_private_test.pem")
+	privKey, _ := jwt.ParseRSAPrivateKeyFromPEM(priv)
+	pub, _ := ioutil.ReadFile("../../rsa_public_test.pem")
+	pubKey, _ := jwt.ParseRSAPublicKeyFromPEM(pub)
+	secret := "anotsorandomtestsecret"
+
+	// Instantiate a common token service to be used by all tests
+	tokenService := services.NewTokenService(&services.TokenServiceConfig{
+		PrivKey:       privKey,
+		PubKey:        pubKey,
+		RefreshSecret: secret,
+	})
+
+	// Include password to make sure it is not serialized
+	uid, _ := uuid.NewRandom()
+	u := &models.User{
+		UID:      uid,
+		Email:    "bob@bob.com",
+		Password: "blarghedymcblarghface",
+	}
+
+	t.Run("Returns a token pair with proper values", func(t *testing.T) {
+		ctx := context.TODO()
+		tokenPair, err := tokenService.NewPairFromUser(ctx, u, "")
+		assert.NoError(t, err)
+
+		var s string
+		assert.IsType(t, s, tokenPair.IDToken)
+
+		// Decode the Base64URL encoded string
+		idTokenClaims := &auth.IDTokenCustomClaims{}
+
+		_, err = jwt.ParseWithClaims(tokenPair.IDToken, idTokenClaims, func(token *jwt.Token) (interface{}, error) {
+			return pubKey, nil
+		})
+
+		assert.NoError(t, err)
+
+		expectedClaims := []interface{}{
+			u.UID,
+			u.Email,
+			u.Name,
+			u.ImageURL,
+			u.Website,
+		}
+		actualIDClaims := []interface{}{
+			idTokenClaims.User.UID,
+			idTokenClaims.User.Email,
+			idTokenClaims.User.Name,
+			idTokenClaims.User.ImageURL,
+			idTokenClaims.User.Website,
+		}
+
+		assert.ElementsMatch(t, expectedClaims, actualIDClaims)
+		assert.Empty(t, idTokenClaims.User.Password) // Password should never be encoded to json
+
+		expiresAt := time.Unix(idTokenClaims.StandardClaims.ExpiresAt, 0)
+		expectedExpiresAt := time.Now().Add(15 * time.Minute)
+		assert.WithinDuration(t, expectedExpiresAt, expiresAt, 5*time.Second)
+
+		refreshTokenClaims := &auth.RefreshTokenCustomClaims{}
+		_, err = jwt.ParseWithClaims(tokenPair.RefreshToken, refreshTokenClaims, func(token *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		})
+
+		assert.IsType(t, s, tokenPair.RefreshToken)
+
+		assert.NoError(t, err)
+		assert.Equal(t, u.UID, refreshTokenClaims.UID)
+
+		expiresAt = time.Unix(refreshTokenClaims.StandardClaims.ExpiresAt, 0)
+		expectedExpiresAt = time.Now().Add(3 * 24 * time.Hour)
+		assert.WithinDuration(t, expectedExpiresAt, expiresAt, 5*time.Second)
+	})
+}
