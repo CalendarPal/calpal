c9cae742fc29685d238a18e4f3ded39f15fa04af
Reece Donovan <reecedonovan99@gmail.com>
2021-06-17 16:25:12 +0100

    Implemented unit testing for the /me endpoint and handler

--- account/go.mod
+++ account/go.mod
@@ -3,7 +3,7 @@ module github.com/CalendarPal/calpal-api
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/gin-gonic/gin v1.7.2
 	github.com/go-playground/validator/v10 v10.6.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.2.0
@@ -12,6 +12,7 @@ require (
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/stretchr/testify v1.7.0
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
---
+++ account/tests/mocks/user_service.go
@@ -0,0 +1,35 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/CalendarPal/calpal-api/models"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
+)
+
+// Mock type for models.UserService
+type MockUserService struct {
+	mock.Mock
+}
+
+// Mock of UserService Get
+func (m *MockUserService) Get(ctx context.Context, uid uuid.UUID) (*models.User, error) {
+
+	// Args that will get returned in the tests when a function is called with a uid
+	ret := m.Called(ctx, uid)
+
+	// The first value to return
+	var r0 *models.User
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*models.User)
+	}
+
+	// The second value to return
+	var r1 error
+	if ret.Get(1) != nil {
+		r1 = ret.Get(1).(error)
+	}
+
+	return r0, r1
+}
