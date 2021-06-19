35adb2dcdf4793bc74c4e55f6ed215c86cc2b217
Reece Donovan <reecedonovan99@gmail.com>
2021-06-19 05:04:57 +0100

    Implemented redis config and initialization

--- account/data_sources.go
+++ account/data_sources.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
 type dataSources struct {
-	DB *sqlx.DB
+	DB          *sqlx.DB
+	RedisClient *redis.Client
 }
 
 // InitDS establishes connections to fields in dataSources
@@ -39,8 +42,27 @@ func initDS() (*dataSources, error) {
 		return nil, fmt.Errorf("error connecting to db: %w", err)
 	}
 
+	// Initialize redis connection
+	redisHost := os.Getenv("REDIS_HOST")
+	redisPort := os.Getenv("REDIS_PORT")
+
+	log.Printf("Connecting to Redis\n")
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", redisHost, redisPort),
+		Password: "",
+		DB:       0,
+	})
+
+	// Verify redis connection
+	_, err = rdb.Ping(context.Background()).Result()
+
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to redis: %w", err)
+	}
+
 	return &dataSources{
-		DB: db,
+		DB:          db,
+		RedisClient: rdb,
 	}, nil
 }
 
@@ -48,6 +70,10 @@ func initDS() (*dataSources, error) {
 func (d *dataSources) close() error {
 	if err := d.DB.Close(); err != nil {
 		return fmt.Errorf("error closing Postgresql: %w", err)
+	}
+
+	if err := d.RedisClient.Close(); err != nil {
+		return fmt.Errorf("error closing Redis Client: %w", err)
 	}
 
 	return nil
--- account/go.mod
+++ account/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.2
 	github.com/go-playground/validator/v10 v10.6.1
+	github.com/go-redis/redis/v8 v8.10.0
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.2.0
 	github.com/jmoiron/sqlx v1.3.4
