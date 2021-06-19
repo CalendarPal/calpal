8677423756f23beef03025de27c4a0983ce3cc42
Reece Donovan <reecedonovan99@gmail.com>
2021-06-19 06:06:57 +0100

    Added user signin to the user service mock

--- account/tests/mocks/user_service.go
+++ account/tests/mocks/user_service.go
@@ -46,3 +46,16 @@ func (m *MockUserService) Signup(ctx context.Context, u *models.User) error {
 
 	return r0
 }
+
+// Mock of UserService Signin
+func (m *MockUserService) Signin(ctx context.Context, u *models.User) error {
+
+	ret := m.Called(ctx, u)
+
+	var r0 error
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(error)
+	}
+
+	return r0
+}
