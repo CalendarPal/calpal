3f1822d6c3d3b47353dff605dce7d1620c94fa96
Reece Donovan <reecedonovan99@gmail.com>
2021-06-19 09:34:05 +0100

    Added models for IDToken and RefreshToken

--- account/models/tokens.go
+++ account/models/tokens.go
@@ -1,7 +1,21 @@
 package models
 
-// TokenPair Used for returning pairs of ID and Refresh tokens
+import "github.com/google/uuid"
+
+// RefreshToken stores token properties that are accessed in multiple application layers
+type RefreshToken struct {
+	ID  uuid.UUID `json:"-"`
+	UID uuid.UUID `json:"-"`
+	SS  string    `json:"refreshToken"`
+}
+
+// IDToken stores token properties that are accessed in multiple application layers
+type IDToken struct {
+	SS string `json:"idToken"`
+}
+
+// TokenPair used for returning pairs of id and refresh tokens
 type TokenPair struct {
-	IDToken      string `json:"idToken"`
-	RefreshToken string `json:"refreshToken"`
+	IDToken
+	RefreshToken
 }
