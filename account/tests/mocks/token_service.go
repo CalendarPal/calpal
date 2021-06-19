423bd03bd01c5dcc7d8ba1a63c5a9247ba8a0bbf
Reece Donovan <reecedonovan99@gmail.com>
2021-06-19 08:41:01 +0100

    Added ID Token validation to the token service mocks

--- account/tests/mocks/token_service.go
+++ account/tests/mocks/token_service.go
@@ -31,3 +31,23 @@ func (m *MockTokenService) NewPairFromUser(ctx context.Context, u *models.User, 
 
 	return r0, r1
 }
+
+// Mock of ValidateIDToken
+func (m *MockTokenService) ValidateIDToken(tokenString string) (*models.User, error) {
+
+	ret := m.Called(tokenString)
+
+	// The first value to return
+	var r0 *models.User
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*models.User)
+	}
+
+	// The second value to return
+	var r1 error
+	if ret.Get(1) != nil {
+		r1 = ret.Get(1).(error)
+	}
+
+	return r0, r1
+}
