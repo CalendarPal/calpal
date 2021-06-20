8bebc39be00db9eba796cae624b3c7dc0f96fb2d
Reece Donovan <reecedonovan99@gmail.com>
2021-06-20 23:22:59 +0100

    Created the user signout handler

---
+++ account/handlers/signout_handler.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"github.com/CalendarPal/calpal-api/account/models"
+	"github.com/CalendarPal/calpal-api/account/utils/apperrors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// Signout handler
+func (h *Handler) Signout(c *gin.Context) {
+	user := c.MustGet("user")
+
+	ctx := c.Request.Context()
+	if err := h.TokenService.Signout(ctx, user.(*models.User).UID); err != nil {
+		c.JSON(apperrors.Status(err), gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "user signed out successfully",
+	})
+}
