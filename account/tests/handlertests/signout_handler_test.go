c34ed8330ef792863cd60ee8d35bd5a68358586a
Reece Donovan <reecedonovan99@gmail.com>
2021-06-20 23:27:51 +0100

    Test for the signout handler

---
+++ account/tests/handlertests/signout_handler_test.go
@@ -0,0 +1,87 @@
+package handlertests
+
+import (
+	"encoding/json"
+	"github.com/CalendarPal/calpal-api/account/handlers"
+	"github.com/CalendarPal/calpal-api/account/models"
+	"github.com/CalendarPal/calpal-api/account/tests/mocks"
+	"github.com/CalendarPal/calpal-api/account/utils/apperrors"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestSignout(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	t.Run("Success", func(t *testing.T) {
+		uid, _ := uuid.NewRandom()
+
+		ctxUser := &models.User{
+			UID:   uid,
+			Email: "bob1@bob.com",
+		}
+
+		// Http response recorder
+		rr := httptest.NewRecorder()
+
+		router := gin.Default()
+		router.Use(func(c *gin.Context) {
+			c.Set("user", ctxUser)
+		})
+
+		mockTokenService := new(mocks.MockTokenService)
+		mockTokenService.On("Signout", mock.AnythingOfType("*context.emptyCtx"), ctxUser.UID).Return(nil)
+
+		handlers.NewHandler(&handlers.Config{
+			R:            router,
+			TokenService: mockTokenService,
+		})
+
+		request, _ := http.NewRequest(http.MethodPost, "/signout", nil)
+		router.ServeHTTP(rr, request)
+
+		respBody, _ := json.Marshal(gin.H{
+			"message": "user signed out successfully!",
+		})
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+		assert.Equal(t, respBody, rr.Body.Bytes())
+	})
+
+	t.Run("Signout Error", func(t *testing.T) {
+		uid, _ := uuid.NewRandom()
+
+		ctxUser := &models.User{
+			UID:   uid,
+			Email: "bob2@bob.com",
+		}
+
+		// Http response recorder
+		rr := httptest.NewRecorder()
+
+		router := gin.Default()
+		router.Use(func(c *gin.Context) {
+			c.Set("user", ctxUser)
+		})
+
+		mockTokenService := new(mocks.MockTokenService)
+		mockTokenService.
+			On("Signout", mock.AnythingOfType("*context.emptyCtx"), ctxUser.UID).
+			Return(apperrors.NewInternal())
+
+		handlers.NewHandler(&handlers.Config{
+			R:            router,
+			TokenService: mockTokenService,
+		})
+
+		request, _ := http.NewRequest(http.MethodPost, "/signout", nil)
+		router.ServeHTTP(rr, request)
+
+		assert.Equal(t, http.StatusInternalServerError, rr.Code)
+	})
+}
