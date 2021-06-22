4bf62092daebe3e19887db8f16554ec150d532e5
Reece Donovan <reecedonovan99@gmail.com>
2021-06-22 08:58:25 +0100

    Basic login/signup screen frontend UI
    Signed-off-by: Reece Donovan <reecedonovan99@gmail.com>

--- account-client/vite.config.js
+++ account-client/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
+/**
+ * https://vitejs.dev/config/
+ * @type {import('vite').UserConfig}
+ */
+export default {
   base: '/account/',
   server: {
     host: true,
   },
   plugins: [vue()],
-});
+};
