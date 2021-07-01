b4bcb31aae014309aec0c66cd4898c174e2911bd
Reece Donovan <reecedonovan99@gmail.com>
2021-07-01 11:33:28 +0100

    Added project list response and repo methods to the interface
    Signed-off-by: Reece Donovan <reecedonovan99@gmail.com>

--- tasks/src/services/interfaces.ts
+++ tasks/src/services/interfaces.ts
@@ -1,10 +1,28 @@
+import { Project } from "../models/project";
 import { Task } from "../models/task";
 import { User } from "../models/user";
+
+export interface ProjectListResponse {
+  tasks: Task[];
+  count: number;
+}
+
+export interface ProjectRepository {
+  create(p: Project): Promise<Project>;
+  getByUser(options: {
+    uid: string;
+    limit: number;
+    offset: number;
+  }): Promise<ProjectListResponse>;
+  deleteByIds(projectId: string[]): Promise<string[]>;
+  update(p: Project): Promise<Project>;
+}
 
 export interface TaskListResponse {
   tasks: Task[];
   count: number;
 }
+
 export interface TaskRepository {
   create(t: Task): Promise<Task>;
   getByUser(options: {
