b3c96c28fefdd3f9aab970c448ff69e827b7c179
Reece Donovan <reecedonovan99@gmail.com>
2021-07-19 12:12:20 +0100

    Implemented basic update and delete mutations for tasks
    Signed-off-by: Reece Donovan <reecedonovan99@gmail.com>

--- tasks/src/graphql/resolvers/task.ts
+++ tasks/src/graphql/resolvers/task.ts
@@ -1,5 +1,5 @@
 import { Task } from "../../database/models/Task";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { MyContext } from "../utils/types";
 
 @Resolver()
@@ -15,5 +15,42 @@ export class TaskResolver {
     @Ctx() { em }: MyContext
   ): Promise<Task | null> {
     return em.findOne(Task, { id });
+  }
+
+  @Mutation(() => Task)
+  async createTask(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Task> {
+    const task = em.create(Task, { title });
+    await em.persistAndFlush(task);
+    return task;
+  }
+
+  @Mutation(() => Task, { nullable: true })
+  async updateTask(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Task | null> {
+    const task = await em.findOne(Task, { id });
+    if (!task) {
+      return null;
+    }
+
+    if (typeof title !== "undefined") {
+      task.title = title;
+      await em.persistAndFlush(task);
+    }
+    return task;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteTask(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Task, { id });
+    return true;
   }
 }
