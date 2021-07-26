79a7f46e09c008e2443772a13666f738f673a433
ReeceDonovan <reecedonovan99@gmail.com>
2021-07-26 14:11:34 +0100

    Adding vite and bulma scss, switching to yarn
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- tasks-client/vite.config.ts
+++ tasks-client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  base: "/",
+  server: {
+    host: true,
+  },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
+});
