57d3abe4d7c31f9ffe8eef64c9347ae204e0abde
ReeceDonovan <reecedonovan99@gmail.com>
2021-08-07 19:37:57 +0100

    Created types for each entity to be used on client side type safety
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

---
+++ project-client/src/types.ts
@@ -0,0 +1,47 @@
+export interface User {
+  createdAt: string;
+  email: string;
+  imageUrl: string;
+  name: string;
+  uid: string;
+  updatedAt: string;
+  website: string;
+}
+
+export interface Project {
+  id: string;
+  title: string;
+  description?: string;
+  projectId: string;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+  tasks: Task[];
+  // Virtual fields
+  taskCount?: number;
+}
+
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  projectId: string;
+  userId: string;
+  startDate: string;
+  goalDate: string;
+  createdAt: string;
+  updatedAt: string;
+  project: Project;
+  // Virtual fields
+  url: string;
+  noteCount?: number;
+}
+
+export interface Note {
+  id: string;
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+  task?: Task;
+  // Virtuals
+}
