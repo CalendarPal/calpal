9dae47769a560b35e3a47e3630c299c75d7070eb
ReeceDonovan <reecedonovan99@gmail.com>
2021-08-07 19:40:26 +0100

    Fixed routing error
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- account-client/vite.config.js
+++ account-client/vite.config.js
@@ -1,11 +1,11 @@
-import vue from '@vitejs/plugin-vue';
+import vue from "@vitejs/plugin-vue";
 
 /**
  * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
 export default {
-  base: '/account/',
+  base: "/account/",
   server: {
     host: true,
   },
